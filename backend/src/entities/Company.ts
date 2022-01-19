3624c89aca5cc5ee15fb6da9fe38ac025f8ef863
Matheus Parro <mathparro@gmail.com>
2022-01-19 17:39:00 -0300

    Empresa Entity

---
+++ backend/src/entities/Company.ts
@@ -0,0 +1,14 @@
+import { Company } from '@prisma/client';
+
+export class CompanyEntity implements Company{
+  id: number;
+  cpnj: string;
+  userId: number;
+
+  constructor(props: Omit<CompanyEntity,'id'>) {
+    Object.assign(this,props);
+  }
+
+
+
+}
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from "@prisma/client";
 import { User } from '@prisma/client';
 
 export class UserEntity implements User{
